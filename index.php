81640d02c19addfde3699a967138a66fe5396ce3
Tamires Vianna Vieira <181710089@senacinformatica.br>
2018-12-05 09:41:44 -0200

    index e menu feitos

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Vision Life</title>
+    </head>
+    <body>
+        
+        <?php
+            require_once 'menu.php';
+        ?>
+        
+        <br> <br> <br> <br> 
+        
+        <h1 align="center">Vision Life Oftalmologia</h1>
+        
+        <?php
+            
+        ?>
+    </body>
+</html>
---
+++ menu.php
@@ -0,0 +1,10 @@
+<header>
+    <a href="index.php">
+        <button>Início</button></a>
+    <a href="clientes.php">
+        <button>Clientes</button></a>
+    <a href="cidades.php">
+        <button>Cidades</button></a>
+</header>
+
+<hr>
