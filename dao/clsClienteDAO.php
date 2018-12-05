4f571598558e72fe143b5f2e13ad007b34ae4e2c
Tamires Vianna Vieira <181710089@senacinformatica.br>
2018-12-05 10:53:11 -0200

    clienteDAO criado

---
+++ dao/clsClienteDAO.php
@@ -0,0 +1,16 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of clsClienteDAO
+ *
+ * @author 181710089
+ */
+class clsClienteDAO {
+    //put your code here
+}
