61c26e59f6dfec2ded7f4504d2d330970fd1207d
Christian Da Luz Freitas <181710066@senacinformatica.br>
2019-04-24 12:15:00 -0300

    consulta

--- dao/clsConsultaDAO.php
+++ dao/clsConsultaDAO.php
@@ -72,8 +72,46 @@ class ConsultaDAO {
         
         return $lista;
     }
+     public static function getConsultaById($id){
+//        $sql = " SELECT c.id, DATE_FORMAT(c.data, '%d/%m/%y'), c.valor, p.id, p.nome, m.id, m.nome, h.id, h.hora "
+        $sql = " SELECT c.id, c.data, c.valor, p.id, p.nome, m.id, m.nome, h.id, h.hora "
+             . " FROM consultas c "
+             . " INNER JOIN clientes p ON p.id = c.codCliente "
+             . " INNER JOIN clientes m ON m.id = c.codMedico  "
+             . " INNER JOIN horarios h ON h.id = c.codHorario  "
+             . " WHERE c.id = ".$id 
+             . " ORDER BY c.data DESC ";
+
+
         
-    }
+        $result = Conexao::consultar($sql);
+        list( $codConsulta, $data, $valor, $idPac, $nomePac, $idMed, $nomeMed, $idHorario, $nomeHorario ) = mysqli_fetch_row($result) ;
+            $paciente = new Cliente();
+            $paciente->setId($idPac);
+            $paciente->setNome($nomePac);
+            
+            $medico = new Cliente();
+            $medico->setId($idMed);
+            $medico->setNome($nomeMed);
+            
+            $horario = new Horario();
+            $horario->setId($idHorario);
+            $horario->setHora($nomeHorario);
+            
+            $consulta = new Consulta();
+            $consulta->setId($codConsulta);
+            $consulta->setValor($valor);
+            $consulta->setData($data);
+            $consulta->setHorario($horario);
+            $consulta->setCliente($paciente);
+            $consulta->setMedico($medico);
+  
+            
+        
+        
+        return $consulta;
+    }   
+  }
     
     
    
